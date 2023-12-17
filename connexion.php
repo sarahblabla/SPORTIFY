6d255e2a053e87f167325dbe9df4164f379ed266
Soufiane0003 <151395714+Soufiane0003@users.noreply.github.com>
2023-12-17 20:03:22 +0100

    Add files via upload

---
+++ connexion.php
@@ -0,0 +1,23 @@
+<?php
+// Démarrez la session
+session_start();
+
+$expected_username = 'nom_utilisateur';
+$expected_password = 'mdp';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    if ($username === $expected_username && $password === $expected_password) {
+        // Définir les informations de session
+        $_SESSION['logged_in'] = true;
+        
+        header('Location: acceuil.html');
+        exit();
+    } else {
+        $login_error = 'Identifiants incorrects. Veuillez réessayer.';
+        echo 'Identifiants incorrects. Veuillez réessayer.';
+    }
+}
+?>
