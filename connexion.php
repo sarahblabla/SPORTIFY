5ce382c41571afd265cfdc4a777ebe1bf4b21266
Soufiane0003 <151395714+Soufiane0003@users.noreply.github.com>
2023-12-17 20:33:20 +0100

    connexion.php

--- connexion.php
+++ connexion.php
@@ -6,13 +6,13 @@ $expected_username = 'nom_utilisateur';
 $expected_password = 'mdp';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $username = $_POST['username'];
-    $password = $_POST['password'];
+    $username = isset($_POST['username']) ? $_POST['username'] : '';
+    $password = isset($_POST['password']) ? $_POST['password'] : '';
 
     if ($username === $expected_username && $password === $expected_password) {
         // Définir les informations de session
         $_SESSION['logged_in'] = true;
-        
+
         header('Location: acceuil.html');
         exit();
     } else {
