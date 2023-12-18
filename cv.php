98b6c04d41846f362629fc2da8147e768325060f
sarahblabla <sarahshahin10@gmail.com>
2023-12-18 21:38:15 +0100

    essai1212

--- cv.php
+++ cv.php
@@ -3,8 +3,40 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="cv.css" rel="stylesheet" type="text/css" />
     <title>CV du Coach</title>
-    <!-- Ajoutez ici vos liens vers les feuilles de style CSS, le cas échéant -->
+
+
+    <div id="logo">
+        <h1>
+            <img src="sportify.png" alt="Musculation" width="200" height="100">
+        </h1>
+
+    </div>
+    <div class="banner">
+        <span>Bienvenue sur Votre Site !</span>
+        <div class="login-form">
+            <a href="#login">Se connecter</a>
+            <a href="#signup">S'inscrire</a>
+        </div>
+    </div>
+
+    <div id="header">
+        <h1>
+
+            Sportify: Activité Sportive  </h1>
+
+    </div>
+
+
+
+    <div id="nav">
+        <a href="acceuil.html" class="nav-button">Acceuil</a>
+        <a href="tout_parcourir.html" class="nav-button">Tout Parcourir</a>
+        <a href="rechercher.html" class="nav-button">Rechercher</a>
+        <a href="rdv.php" class="nav-button">Rendez-Vous</a>
+        <a href="votre_compte.html" class="nav-button">Votre Compte</a>
+    </div>
 </head>
 <body>
 
@@ -26,10 +58,10 @@ die("La connexion a échoué : " . $conn->connect_error);
 }
 
 // Récupérez l'ID du coach depuis la page précédente (assurez-vous de sécuriser vos entrées contre les attaques par injection SQL)
-$id_coach = $_GET['id_coach'];
+$idCoach = $_GET['idCoach'];
 
 // Requête SQL pour récupérer les informations du coach
-$sql = "SELECT * FROM coach WHERE idcoach = $id_coach";
+$sql = "SELECT * FROM coach WHERE idcoach = $idCoach";
 $result = $conn->query($sql);
 
 // Vérifiez si la requête a renvoyé des résultats
@@ -46,20 +78,16 @@ echo "<h1>CV de $prenom $nom</h1>";
 // Vérifiez si le fichier CV est une image JPG
     if (pathinfo($CV, PATHINFO_EXTENSION) === 'jpg') {
         // Spécifiez la largeur et la hauteur souhaitées
-        $largeur = 1400; // Remplacez par la largeur souhaitée en pixels
-        $hauteur = 2000; // Remplacez par la hauteur souhaitée en pixels
+        $largeur = 700; // Remplacez par la largeur souhaitée en pixels
+        $hauteur = 1000; // Remplacez par la hauteur souhaitée en pixels
 
         // Affichez l'image avec les attributs de largeur et hauteur
-        echo "<img src='$CV' alt='CV de $prenom $nom' width='$largeur' height='$hauteur'>";
+        echo "<img src='$CV' alt='CV de $prenom $nom' width='$largeur' height='$hauteur',text-align='center'>";
     } else {
         // Si ce n'est pas un fichier JPG, affichez le lien vers le fichier
         echo "<a href='$CV'>Télécharger le CV</a>";
     }
 
-// ... (suite de votre code)
-
-
-// Vous pouvez ajouter d'autres informations ici en fonction de votre base de données
 } else {
 echo "Aucun coach trouvé avec cet ID.";
 }
